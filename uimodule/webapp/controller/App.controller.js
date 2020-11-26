9d7c0333b531ac20ab8db3cb688923a8bd41d0d1
Уколов Сергей Игоревич <si.ukolov@severstal.com>
2020-11-26 16:58:11 +0300

    test view

--- uimodule/webapp/controller/App.controller.js
+++ uimodule/webapp/controller/App.controller.js
@@ -1,7 +1,14 @@
-sap.ui.define([
-  "com/ui5/ui5list/controller/BaseController"
-], function(Controller) {
-  "use strict";
+sap.ui.define(
+  ["com/ui5/ui5list/controller/BaseController",
+  "sap/ui/model/json/JSONModel"],
+  function (Controller, JSONModel) {
+    "use strict";
 
-  return Controller.extend("com.ui5.ui5list.controller.App", {});
-});
+    return Controller.extend("com.ui5.ui5list.controller.App", {
+      onInit: function () {
+
+        var oModel = new JSONModel(sap.ui.require.toUrl("com/ui5/ui5list/model/List.json"));
+        this.getView().setModel(oModel);
+    }
+  });
+  });
